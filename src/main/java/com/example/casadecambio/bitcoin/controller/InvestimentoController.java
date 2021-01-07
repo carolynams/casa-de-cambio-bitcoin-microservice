5cb8d06f2d3b2366102556f13e2a74d96d50ae36
Carolyna <Carolynacaarolmantovani@hotmail.com>
2021-01-07 12:07:02 -0300

    Alterado o tipo do parametro a ser encaminhado na api

--- src/main/java/com/example/casadecambio/bitcoin/controller/InvestimentoController.java
+++ src/main/java/com/example/casadecambio/bitcoin/controller/InvestimentoController.java
@@ -3,10 +3,7 @@ package com.example.casadecambio.bitcoin.controller;
 import com.example.casadecambio.bitcoin.model.Investimento;
 import com.example.casadecambio.bitcoin.service.InvestimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -16,8 +13,8 @@ public class InvestimentoController {
     @Autowired
     private InvestimentoService service;
 
-    @GetMapping("/")
-    public Mono<Investimento> getLucro(@RequestParam String cpf) {
+    @GetMapping("/{cpf}")
+    public Mono<Investimento> getLucro(@PathVariable String cpf) {
         return service.getValorInvestido(cpf);
     }
 }
